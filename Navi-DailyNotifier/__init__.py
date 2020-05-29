d71ebc3a3cf4c3b4d5e2e958ab267f8327b16962
Joe Markiewicz <jmarkiewicz.ext@batteriesplus.com>
2020-05-29 11:27:17 -0500

    performance update

--- Navi-DailyNotifier/__init__.py
+++ Navi-DailyNotifier/__init__.py
@@ -6,19 +6,19 @@ import yfinance as yf
 import azure.functions as func
 from datetime import datetime, timedelta
 
-navihost = '{navihost}'
-navidb = '{navidb}'
-naviuser = '{naviuser}'
-navipwd = '{navipwd}'
-naviport = '{naviport}'
-navimail = '{navimail}'
-navimailpwd = '{navimailpwd}'
+navihost='{navihost}'
+navidb='{navidb}'
+naviuser='{naviuser}'
+navipwd='{navipwd}'
+naviport='{naviport}'
+navimail='{navimail}'
+navimailpwd='{navimailpwd}'
 
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.today()
     def stock():
-        #################################################################################################
-        # Below is defining stock market holidays and days of the week to get previous stock quotes.
+    #################################################################################################
+    # Below is defining stock market holidays and days of the week to get previous stock quotes.
         offday = ['07-03-20', '09-07-20', '11-26-20', '12-25-20']
 
         # Define datetime for today and yesterday (or holidays/weekends) to get change in stock.
@@ -27,8 +27,23 @@ def main(mytimer: func.TimerRequest) -> None:
         holiday = datetime.today() - timedelta(2)
         friday = datetime.today() - timedelta(3)
         holimonday = datetime.today() - timedelta(4)
-        #################################################################################################
-        # Below is establishing connection to Postgres DB and executing select queries.
+
+        #Used to define the date used in the yfinance call
+        if yesterday.strftime('%m-%d-%y') in offday and yesterday.weekday() == 0:
+            current = today
+            prior = holimonday
+        elif yesterday.strftime('%m-%d-%y') in offday:
+            current = today
+            prior = holiday
+        elif today.weekday() == 0:
+            current = today
+            prior = friday
+        else:
+            current = today
+            prior = yesterday
+
+    #################################################################################################
+    # Below is establishing connection to Postgres DB and executing select queries.
         stock_dict = {}
 
         con = psycopg2.connect(
@@ -52,7 +67,6 @@ def main(mytimer: func.TimerRequest) -> None:
 
         cur.execute(user_call)
         user = cur.fetchall()
-        #print(user)
 
         cur.execute(stock_ticker)
         tick = cur.fetchall()
@@ -63,188 +77,52 @@ def main(mytimer: func.TimerRequest) -> None:
             else:
                 stock_dict[column[0]] = {column[1]: (column[2], column[3])}
 
-        # Below creates the stock list from the second select statement.
+    # Below creates the stock list from the second select statement.
         ticker = [row[0] for row in tick]
-        
-        #print(ticker)
-    
-        # If statements to execute based off what type of day .today() is.
-        if yesterday.strftime('%m-%d-%y') in offday and yesterday.weekday() == 0:
-            stock_live = {i: float(round(yf.download(i,today,today)['Adj Close'].iloc[0],2))for i in ticker}
-            stock_prev = {i: float(round(yf.download(i,holimonday,holimonday)['Adj Close'].iloc[0], 2)) for i in ticker}
 
-            for stock_integer in stock_dict:
-                for actual in stock_dict[stock_integer]:
-                    stock_dict[stock_integer][actual] = (stock_live.get(actual), stock_prev.get(actual))
+        stock_live = {i: float(round(yf.download(i,current,current)['Adj Close'].iloc[0],2))for i in ticker}
+        stock_prev = {i: float(round(yf.download(i,prior,prior)['Adj Close'].iloc[0], 2)) for i in ticker}
 
-            for u, s in stock_dict.items():
-                usernumber = u
-                result = []
-                for tick, price in s.items():
-                    stock = tick  # stock ticker
-                    stock_cur = price[0]  # todays stock price
-                    stock_previ = price[1]  # yesterdays stock price
+        for stock_integer in stock_dict:
+            for actual in stock_dict[stock_integer]:
+                stock_dict[stock_integer][actual] = (stock_live.get(actual), stock_prev.get(actual))
 
-                    # Calculating change in stock price from yesterday
-                    stock_net = float(round((stock_cur - stock_previ), 2))
-                    stock_change = float((stock_cur - stock_previ) / stock_previ)
-                    stock_percent = format(stock_change, '.2%')
+        for u, s in stock_dict.items():
+            usernumber = u
+            result = []
+            for tick, price in s.items():
+                stock = tick  # stock ticker
+                stock_cur = price[0]  # todays stock price
+                stock_previ = price[1]  # yesterdays stock price
 
-                    # Creating a final message to be displayed in the email regarding each stock listed in the for loop
-                    stock_message = '\n\n' + stock + ' = $' + str(round(stock_cur, 2)) + ' | $' + str(stock_net) + ' ' + str(stock_percent) + ' from prev close'
+                # Calculating change in stock price from yesterday
+                stock_net = float(round((stock_cur - stock_previ), 2))
+                stock_change = float((stock_cur - stock_previ) / stock_previ)
+                stock_percent = format(stock_change, '.2%')
 
-                    # Added the .append so each iteration of the for loop is included in the 'result' list at the beg of the function.
-                    result.append(stock_message)
+                # Creating a final message to be displayed in the email regarding each stock listed in the for loop
+                stock_message = '\n\n' + stock + ' = $' + str(round(stock_cur, 2)) + ' | $' + str(stock_net) + ' ' + str(stock_percent) + ' from prev close'
 
-                    # Below removes the brackets and quotes that would otherwise be included in returning a list.
-                    finalstock = (', '.join(result))
+                # Added the .append so each iteration of the for loop is included in the 'result' list at the beg of the function.
+                result.append(stock_message)
 
-                # Defing the mail service I am using. In this case it is gmail.
-                mail = smtplib.SMTP('smtp.gmail.com',587)
+                # Below removes the brackets and quotes that would otherwise be included in returning a list.
+                finalstock = (', '.join(result))
 
-                # Starting the logic for smtplib mail to send the information using my gmail to my phone.
-                mail.ehlo()
-                mail.starttls()
-                mail.login(navimail, navimailpwd)
-                # Below (TO) sender pulls from the SQL statement at the begin of the for loop, and the finalstock is pulled from the inner loop
+            # Defing the mail service I am using. In this case it is gmail.
+            mail = smtplib.SMTP('smtp.gmail.com',587)
 
-                message = 'Hey, Listen! Your interested stock price values for today (' + today.strftime('%m-%d-%y') + ') are...' + str(finalstock)
-                mail.sendmail(navimail, usernumber, str(message))
+            # Starting the logic for smtplib mail to send the information using my gmail to my phone.
+            mail.ehlo()
+            mail.starttls()
+            mail.login(navimail, navimailpwd)
+            # Below (TO) sender pulls from the SQL statement at the begin of the for loop, and the finalstock is pulled from the inner loop
 
-                mail.close()
-            
-        elif yesterday.strftime('%m-%d-%y') in offday:
-            stock_live = {i: float(round(yf.download(i,today,today)['Adj Close'].iloc[0],2))for i in ticker}
-            stock_prev = {i: float(round(yf.download(i,holiday,holiday)['Adj Close'].iloc[0], 2)) for i in ticker}
+            message = 'Hey, Listen! \n\nYour interested stock price values for today (' + today.strftime('%m-%d-%y') + ') are...' + str(finalstock)
+            mail.sendmail(navimail, usernumber, str(message))
 
-            for stock_integer in stock_dict:
-                for actual in stock_dict[stock_integer]:
-                    stock_dict[stock_integer][actual] = (stock_live.get(actual), stock_prev.get(actual))
-
-            for u, s in stock_dict.items():
-                usernumber = u
-                result = []
-                for tick, price in s.items():
-                    stock = tick  # stock ticker
-                    stock_cur = price[0]  # todays stock price
-                    stock_previ = price[1]  # yesterdays stock price
-
-                    # Calculating change in stock price from yesterday
-                    stock_net = float(round((stock_cur - stock_previ), 2))
-                    stock_change = float((stock_cur - stock_previ) / stock_previ)
-                    stock_percent = format(stock_change, '.2%')
-
-                    # Creating a final message to be displayed in the email regarding each stock listed in the for loop
-                    stock_message = '\n\n' + stock + ' = $' + str(round(stock_cur, 2)) + ' | $' + str(stock_net) + ' ' + str(stock_percent) + ' from prev close'
-
-                    # Added the .append so each iteration of the for loop is included in the 'result' list at the beg of the function.
-                    result.append(stock_message)
-
-                    # Below removes the brackets and quotes that would otherwise be included in returning a list.
-                    finalstock = (', '.join(result))
-
-                # Defing the mail service I am using. In this case it is gmail.
-                mail = smtplib.SMTP('smtp.gmail.com',587)
-
-                # Starting the logic for smtplib mail to send the information using my gmail to my phone.
-                mail.ehlo()
-                mail.starttls()
-                mail.login(navimail, navimailpwd)
-                # Below (TO) sender pulls from the SQL statement at the begin of the for loop, and the finalstock is pulled from the inner loop
-
-                message = 'Hey, Listen! Your interested stock price values for today (' + today.strftime('%m-%d-%y') + ') are...' + str(finalstock)
-                mail.sendmail(navimail, usernumber, str(message))
-
-                mail.close()
-
-        elif today.weekday() == 0:
-            stock_live = {i: float(round(yf.download(i,today,today)['Adj Close'].iloc[0],2))for i in ticker}
-            stock_prev = {i: float(round(yf.download(i,friday,friday)['Adj Close'].iloc[0], 2)) for i in ticker}
-
-            for stock_integer in stock_dict:
-                for actual in stock_dict[stock_integer]:
-                    stock_dict[stock_integer][actual] = (stock_live.get(actual), stock_prev.get(actual))
-
-            for u, s in stock_dict.items():
-                usernumber = u
-                result = []
-                for tick, price in s.items():
-                    stock = tick  # stock ticker
-                    stock_cur = price[0]  # todays stock price
-                    stock_previ = price[1]  # yesterdays stock price
-
-                    # Calculating change in stock price from yesterday
-                    stock_net = float(round((stock_cur - stock_previ), 2))
-                    stock_change = float((stock_cur - stock_previ) / stock_previ)
-                    stock_percent = format(stock_change, '.2%')
-
-                    # Creating a final message to be displayed in the email regarding each stock listed in the for loop
-                    stock_message = '\n\n' + stock + ' = $' + str(round(stock_cur, 2)) + ' | $' + str(stock_net) + ' ' + str(stock_percent) + ' from prev close'
-
-                    # Added the .append so each iteration of the for loop is included in the 'result' list at the beg of the function.
-                    result.append(stock_message)
-
-                    # Below removes the brackets and quotes that would otherwise be included in returning a list.
-                    finalstock = (', '.join(result))
-
-                # Defing the mail service I am using. In this case it is gmail.
-                mail = smtplib.SMTP('smtp.gmail.com',587)
-
-                # Starting the logic for smtplib mail to send the information using my gmail to my phone.
-                mail.ehlo()
-                mail.starttls()
-                mail.login(navimail, navimailpwd)
-                # Below (TO) sender pulls from the SQL statement at the begin of the for loop, and the finalstock is pulled from the inner loop
-
-                message = 'Hey, Listen! Your interested stock price values for today (' + today.strftime('%m-%d-%y') + ') are...' + str(finalstock)
-                mail.sendmail(navimail, usernumber, str(message))
-
-                mail.close()
-
-        else:
-            stock_live = {i: float(round(yf.download(i,today,today)['Adj Close'].iloc[0],2))for i in ticker}
-            stock_prev = {i: float(round(yf.download(i,yesterday,yesterday)['Adj Close'].iloc[0], 2)) for i in ticker}
-
-            for stock_integer in stock_dict:
-                for actual in stock_dict[stock_integer]:
-                    stock_dict[stock_integer][actual] = (stock_live.get(actual), stock_prev.get(actual))
-
-            for u, s in stock_dict.items():
-                usernumber = u
-                result = []
-                for tick, price in s.items():
-                    stock = tick  # stock ticker
-                    stock_cur = price[0]  # todays stock price
-                    stock_previ = price[1]  # yesterdays stock price
-
-                    # Calculating change in stock price from yesterday
-                    stock_net = float(round((stock_cur - stock_previ), 2))
-                    stock_change = float((stock_cur - stock_previ) / stock_previ)
-                    stock_percent = format(stock_change, '.2%')
-
-                    # Creating a final message to be displayed in the email regarding each stock listed in the for loop
-                    stock_message = '\n\n' + stock + ' = $' + str(round(stock_cur, 2)) + ' | $' + str(stock_net) + ' ' + str(stock_percent) + ' from prev close'
-
-                    # Added the .append so each iteration of the for loop is included in the 'result' list at the beg of the function.
-                    result.append(stock_message)
-
-                    # Below removes the brackets and quotes that would otherwise be included in returning a list.
-                    finalstock = (', '.join(result))
-
-                # Defing the mail service I am using. In this case it is gmail.
-                mail = smtplib.SMTP('smtp.gmail.com',587)
-
-                # Starting the logic for smtplib mail to send the information using my gmail to my phone.
-                mail.ehlo()
-                mail.starttls()
-                mail.login(navimail, navimailpwd)
-                # Below (TO) sender pulls from the SQL statement at the begin of the for loop, and the finalstock is pulled from the inner loop
-
-                message = 'Hey, Listen! Your interested stock price values for today (' + today.strftime('%m-%d-%y') + ') are...' + str(finalstock)
-                mail.sendmail(navimail, usernumber, str(message))
-
-                mail.close()
-        print(message)
+            mail.close()
+            print(message)
     stock()
 
     if mytimer.past_due:
